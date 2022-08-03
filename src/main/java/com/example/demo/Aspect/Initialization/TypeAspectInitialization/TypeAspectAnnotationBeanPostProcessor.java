02767468af932061307042722bcb55b408219544
wangwenhao <wangwenhao@dbis.nankai.edu.cn>
2022-08-03 20:24:33 +0800

    feat: 将type注解加入注册中心

---
+++ src/main/java/com/example/demo/Aspect/Initialization/TypeAspectInitialization/TypeAspectAnnotationBeanPostProcessor.java
@@ -0,0 +1,32 @@
+package com.example.demo.Aspect.Initialization.TypeAspectInitialization;
+
+import com.example.demo.Aspect.Interface.FiledAspect;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+
+import java.util.Set;
+
+//这个例子只是将添加了FiledAspect注解的类添加到注册中心里
+public class TypeAspectAnnotationBeanPostProcessor implements BeanDefinitionRegistryPostProcessor {
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        //创建一个扫描器
+        ClassPathScanningCandidateComponentProvider classPathScanningCandidateComponentProvider = new ClassPathScanningCandidateComponentProvider(false);
+        //扫描所有添加了@FiledAspect的 BeanDefinition
+        classPathScanningCandidateComponentProvider.addIncludeFilter(new AnnotationTypeFilter(FiledAspect.class));
+        Set<BeanDefinition> beanDefinitions = classPathScanningCandidateComponentProvider.findCandidateComponents("com.example.demo.Model");
+        for (BeanDefinition beanDefinition:beanDefinitions){
+            registry.registerBeanDefinition(beanDefinition.getBeanClassName(), beanDefinition);
+        }
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+    }
+}
